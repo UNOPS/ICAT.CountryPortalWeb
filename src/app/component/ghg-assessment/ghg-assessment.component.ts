02875897a22078279ae0b0248256d4d45749c432
sandu9618 <sandunipavithra9611@gmail.com>
2022-12-12 11:44:29 +0530

    FIx

--- src/app/component/ghg-assessment/ghg-assessment.component.ts
+++ src/app/component/ghg-assessment/ghg-assessment.component.ts
@@ -3623,9 +3623,9 @@ else{
     param.isAlternative = isAlternative;
 
     if (param.isAlternative) {
-      // if (pp !== undefined){
-      //   pp.institution = param.institution
-      // }
+      if (pp !== undefined && pp.isHistorical){
+        pp.institution = null!
+      }
       param.parentParameter = pp!;
     }
     param.baseYear = this.baseYear.getFullYear();
