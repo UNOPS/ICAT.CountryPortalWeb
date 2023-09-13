ffdf95462ac91e1de04957ca0e6b9962858a6e1f
Pradeep_Ranasinghe <pradeep@climatesi.com>
2023-09-13 14:20:36 +0530

    increase time out

--- src/app/quality-check-detail/quality-check-detail.component.ts
+++ src/app/quality-check-detail/quality-check-detail.component.ts
@@ -457,7 +457,7 @@ export class QualityCheckDetailComponent implements OnInit {
       this.isReadyToCAl = false;
       this.isDisable = true;
       window.location.reload()
-    },1500);
+    },3500);
     }
   }
 
