5881bf4389468cecc5a971f5aadba3cec7010179
Pradeep_Ranasinghe <pradeep@climatesi.com>
2024-05-09 16:26:58 +0530

    doc

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: true,
-  baseUrlAPI: 'http://localhost:8080',
+  baseUrlAPI: 'https://icat-countryportalservice-qa-2sshj5de3a-ey.a.run.app',
   baseUrlAPIDocdownloadAPI:
-    'http://localhost:8080' + '/document/downloadDocument',
-  baseUrlAPIDocUploadAPI: 'http://localhost:8080' + '/document/upload2',
-  baseUrlAPIDocUploadAnonymousAPI: 'http://localhost:8080' + '/document/uploadFileAnonymous',
+    'https://icat-countryportalservice-qa-2sshj5de3a-ey.a.run.app' + '/document/downloadDocument',
+  baseUrlAPIDocUploadAPI: 'https://icat-countryportalservice-qa-2sshj5de3a-ey.a.run.app' + '/document/upload2',
+  baseUrlAPIDocUploadAnonymousAPI: 'https://icat-countryportalservice-qa-2sshj5de3a-ey.a.run.app' + '/document/uploadFileAnonymous',
   baseUrlJsonFile:
     'https://icat-calculationengine-qa-2sshj5de3a-ey.a.run.app/filename',
   baseUrlMac: 'https://icat-calculationengine-qa-2sshj5de3a-ey.a.run.app/mac',
-  baseUrlExcelUpload: 'http://localhost:8080' + '/parameter/upload',
+  baseUrlExcelUpload: 'https://icat-countryportalservice-qa-2sshj5de3a-ey.a.run.app' + '/parameter/upload',
   baseUrlAPIDocReportChartDownloadAPI:
-    'http://localhost:8080' + '/report/chartDataImage',
+    'https://icat-countryportalservice-qa-2sshj5de3a-ey.a.run.app' + '/report/chartDataImage',
   baseUrlPMU: 'https://tracadpmu.unops.org',
   apiKey1: '1234',
   apiKey2: '56789',
